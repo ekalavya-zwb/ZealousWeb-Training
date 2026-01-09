2e0c8ddf88d6d47ebb2db6dbfa0218f99e549404
Ekalavya <kekalavya007@gmail.com>
2026-01-09 18:59:30 +0530

    Completed Day 4: Node.js - HTTP & Express Basics

---
+++ express-app/server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.set("json spaces", 2);
+
+let users = [
+  { id: 1, name: "Ekalavya", email: "ekalavya@gmail.com" },
+  { id: 2, name: "Aman", email: "aman@gmail.com" },
+  { id: 3, name: "Ansh", email: "ansh@gmail.com" },
+];
+
+let products = [
+  { id: 1, name: "Mobile", price: 25000 },
+  { id: 2, name: "Laptop", price: 70000 },
+  { id: 3, name: "Headphone", price: 5000 },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to my express application!");
+});
+
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    users: "/api/users",
+    products: "/api/products",
+  });
+});
+
+app.get("/api/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/api/products", (req, res) => {
+  res.status(200).json(products);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((u) => u.id === userId);
+
+  if (!user) {
+    res.status(404).send("User not found!");
+  }
+
+  res.status(200).json(user);
+});
+
+app.use((req, res) => {
+  res.status(404).send("404 - Page Not Found!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}`);
+});
