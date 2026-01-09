11d405b3ccf7efdba93d13bb1843cdd2ed786836
Ekalavya <kekalavya007@gmail.com>
2026-01-09 19:11:38 +0530

    Completed Day 4: Node.js - HTTP & Express Basics

--- node-basics/http-server.js
+++ node-basics/http-server.js
@@ -1,17 +1,10 @@
 const http = require("http");
+const handleRoutes = require("./http-routes.js");
+
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/" && req.method === "GET") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Welcome to my Home page!</h1>");
-  } else if (req.url === "/about" && req.method === "GET") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.end("<h1>Welcome to my About page!</h1>");
-  } else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.end("<h1>404 - Page Not Found</h1>");
-  }
+  handleRoutes(req, res);
 });
 
 server.listen(PORT, () => {
